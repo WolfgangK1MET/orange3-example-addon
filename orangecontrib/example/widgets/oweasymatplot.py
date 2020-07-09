1d01e06fad88ec6dff08f19c2581cdf7e3b89401
k1-simonb <67892471+k1-simonb@users.noreply.github.com>
2020-07-09 12:11:39 +0200

    Update oweasymatplot.py

--- orangecontrib/example/widgets/oweasymatplot.py
+++ orangecontrib/example/widgets/oweasymatplot.py
@@ -1,12 +1,17 @@
 import Orange
 
-from Orange.data import Table
+from Orange.data import Table, ContinuousVariable
 from Orange.widgets.widget import OWWidget, Input, Output, Msg
 from Orange.data import TimeVariable, Domain
 from Orange.widgets import gui
+from Orange.widgets.utils.itemmodels import DomainModel
+from pyqtgraph.widgets.MatplotlibWidget import MatplotlibWidget
+import dateutil.parser
+import matplotlib
+import datetime
+import random
 
 from AnyQt.QtCore import Qt
-
 
 class OWEasyMatplot(OWWidget):
     name = "Easymatplot-test"
@@ -34,9 +39,35 @@ class OWEasyMatplot(OWWidget):
         
         common_options = dict(labelWidth=50, orientation=Qt.Horizontal, sendSelectedValue=True, contentsLength=14)
         
-        self.attr_box = gui.vBox(self.controlArea, True)
-        self.x_axes = []
+        # self.__dict__[...]
         
+        self.attr_box = gui.vBox(self.controlArea, True)
+        self.attr_x = None
+        self.attr_y = None
+        
+        dmod = DomainModel
+        self.x_model = DomainModel(dmod.MIXED, valid_types=TimeVariable)
+        self.y_model = DomainModel(dmod.MIXED, valid_types=ContinuousVariable)
+        
+        self.axis_box = gui.vBox(self.attr_box, True)
+        self.cb_attr_x = gui.comboBox(self.axis_box, self, "attr_x", label="Axis x:", callback=self.set_attr_x_from_combo, model=self.x_model, **common_options, searchable = True)
+        self.cb_attr_y = gui.comboBox(self.axis_box, self, "attr_y", label="Axis y:", callback=self.set_attr_y_from_combo, model=self.y_model, **common_options, searchable = True)
+        
+        self.graph = MatplotlibWidget()
+        
+        box = gui.vBox(self.mainArea, True, margin=0)
+        box.layout().addWidget(self.graph)
+        self.subplot = self.graph.getFigure().add_subplot()
+        
+        self.show()
+        
+    # Callback function for cb_attr_x
+    def set_attr_x_from_combo(self):
+        self.__update_plot()
+    
+    # Callback function for cb_attr_y
+    def set_attr_y_from_combo(self):
+        self.__update_plot()
 
     @Inputs.data
     def set_data(self, dataset):
@@ -45,14 +76,55 @@ class OWEasyMatplot(OWWidget):
         
         if dataset is not None:
             self.Warning.empty_data(shown = False)
-            self.__detect_time_variable()
-        else:
-            self.Output.selected.send(self.__input_data)
+            time_var = self.__detect_time_variable()
+            
+            self.x_model.set_domain(dataset.domain)
+            self.y_model.set_domain(dataset.domain)
+            self.attr_x = time_var 
+            self.attr_y = self.y_model[1] # check if there is one ... // method needed which get first number type ...
+            
+        self.Outputs.selected.send(self.__input_data)
     
     def __detect_time_variable(self):
         time_var = TableUtility.get_first_time_variable(self.__input_data)
         
+        return time_var
+    
+    def __update_plot(self):
+        self.selected = self.__input_data[:, self.attr_y] # Wie, wenn mehrere attr_y?
         
+        self.__setup_plot()
+        self.__commit()
+        
+    def __setup_plot(self):
+        if self.subplot is not None:
+            self.subplot.clear()
+            yLabel = ""
+            
+            if self.selected is not None:
+                print(self.selected)
+                self.subplot.plot(self.selected, label=self.attr_y.name)
+                ylabel = self.attr_y.name;
+                
+            x_names = []
+            
+            for row in self.__input_data:
+                x_names.append(dateutil.parser.parse(f'{row["DatumUhrzeit"]}'))
+                print(dateutil.parser.parse(f'{row["DatumUhrzeit"]}'))
+                
+            for i in self.selected:
+                print(i)
+            
+            x = [datetime.datetime.now() + datetime.timedelta(hours=i) for i in range(12)]
+            y = [i+random.gauss(0,1) for i,_ in enumerate(x)]
+            
+            self.subplot.set_xtick()
+  
+    def __commit(self):
+        self.subplot.legend()
+        self.graph.draw()
+        
+        self.Outputs.selected.send(self.selected)
         
 class TableUtility:
     @staticmethod
