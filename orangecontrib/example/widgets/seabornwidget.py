d9a9e33b0341b83d468551ce64ee42edb04d4401
k1-simonb <67892471+k1-simonb@users.noreply.github.com>
2020-07-14 16:00:36 +0200

    Create seabornwidget.py

---
+++ orangecontrib/example/widgets/seabornwidget.py
@@ -0,0 +1,32 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from AnyQt.QtCore import Qt
+import matplotlib
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from PyQt5 import QtCore, QtGui, QtWidgets
+from AnyQt.QtCore import Qt
+
+class SeabornWidget(QtWidgets.QWidget):
+    def __init__(self, size=(5.0, 4.0), dpi=100):
+        QtWidgets.QWidget.__init__(self)
+        self.fig = Figure(size, dpi=dpi)
+        self.canvas = FigureCanvas(self.fig)
+        self.canvas.setParent(self)
+        self.toolbar = NavigationToolbar(self.canvas, self)
+
+        self.vbox = QtGui.QVBoxLayout()
+        self.vbox.addWidget(self.toolbar)
+        self.vbox.addWidget(self.canvas)
+
+        self.setLayout(self.vbox)
+
+    def getFigure(self):
+        return self.fig
+
+    def draw(self):
+        self.canvas.draw()
+
