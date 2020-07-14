6b7abec558157fc1fd32c9248a12198acb9c0d74
k1-simonb <67892471+k1-simonb@users.noreply.github.com>
2020-07-14 16:00:42 +0200

    Update seaborn-test.py

--- orangecontrib/example/widgets/seaborn-test.py
+++ orangecontrib/example/widgets/seaborn-test.py
@@ -6,28 +6,40 @@ import Orange
 
 from Orange.data import Table, ContinuousVariable
 from Orange.widgets.widget import OWWidget, Input, Output, Msg
-from Orange.data import TimeVariable
-from Orange.widgets import gui
-from Orange.widgets.utils.itemmodels import DomainModel
-from pyqtgraph.widgets.MatplotlibWidget import MatplotlibWidget
-import dateutil.parser
-import matplotlib.dates as mdates
-
-from AnyQt.QtCore import Qt
-from PyQt4 import QtGui
-import pandas as pd
 from matplotlib import pyplot as plt
-from pylab import rcParams
-import sys
-import time
-from matplotlib.backends.qt_compat import QtCore, QtWidgets, is_pyqt5
-import numpy as np
-
-from PyQt5 import QtCore, QtGui, QtWidgets
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-import sys
 import seaborn as sns
+from Orange.widgets import gui
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from PyQt5 import QtCore, QtGui, QtWidgets
+from AnyQt.QtCore import Qt
+from pyqtgraph.widgets.MatplotlibWidget import MatplotlibWidget
+
+class SeabornWidget2(QtWidgets.QWidget):
+    def __init__(self, size=(5.0, 4.0), dpi=100):
+        QtWidgets.QWidget.__init__(self)
+        self.fig = Figure(size, dpi=dpi)
+        self.canvas = FigureCanvas(self.fig)
+        self.canvas.setParent(self)
+        self.toolbar = NavigationToolbar(self.canvas, self)
+
+        self.vbox = QtGui.QVBoxLayout()
+        self.vbox.addWidget(self.toolbar)
+        self.vbox.addWidget(self.canvas)
+
+        self.setLayout(self.vbox)
+
+    def getFigure(self):
+        return self.fig
+
+    def draw(self):
+        self.canvas.draw()
+
+
 
 tips = sns.load_dataset("tips")
 
@@ -43,6 +55,7 @@ class OWSeabornTest(OWWidget,QtWidgets.QMainWindow):
     description = "A widget for easy plots with matplotlib"
     icon = "icons/oweasyplot.svg"
     want_main_area = True
+    want_control_area = True
     
     class Inputs:
         data = Input("Data", Orange.data.Table)
@@ -58,57 +71,58 @@ class OWSeabornTest(OWWidget,QtWidgets.QMainWindow):
         
     def __init__(self):
         super().__init__()
-        self.main_widget = QtGui.QWidget(self)
-
-        self.graph = Figure()
-        self.ax1 = self.graph.add_subplot(121)
-        self.ax2 = self.graph.add_subplot(122, sharex=self.ax1, sharey=self.ax1)
-        self.axes=[self.ax1, self.ax2]
-        self.graph = FigureCanvas(self.graph)
-
-        self.graph.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
-        self.graph.updateGeometry()
-        
-        self.dropdown1 = QtGui.QComboBox()
+        self.graph = MatplotlibWidget()
+        box = gui.vBox(self.mainArea, True, margin=0)
+        box.layout().addWidget(self.graph)
+        self.ax1 = self.graph.getFigure().add_subplot(121)
+        self.ax2 = self.graph.getFigure().add_subplot(122, sharex=self.ax1, sharey=self.ax1)
+        self.axes = [self.ax1, self.ax2]
+        self.canvas = FigureCanvas(self.graph.getFigure())
+        self.dropdown1 = QtWidgets.QComboBox()
         self.dropdown1.addItems(["sex", "time", "smoker"])
-        self.dropdown2 = QtGui.QComboBox()
+        self.dropdown2 = QtWidgets.QComboBox()
         self.dropdown2.addItems(["sex", "time", "smoker", "day"])
         self.dropdown2.setCurrentIndex(2)
-        
+
+
+
+        layout = QtWidgets.QGridLayout(self.controlArea)
+        layout.addWidget(QtWidgets.QLabel("Select category for subplots"))
+        layout.addWidget(self.dropdown1)
+        layout.addWidget(QtWidgets.QLabel("Select category for markers"))
+        layout.addWidget(self.dropdown2)
+
         self.dropdown1.currentIndexChanged.connect(self.update)
         self.dropdown2.currentIndexChanged.connect(self.update)
 
-        self.label = QtGui.QLabel("A plot:")
+        self.left_side_scrolling = True
 
-
-        self.show()
         self.update()
+        self.show()
+        self.graph.draw()
 
     @Inputs.data
     def set_input_data(self, data):
         self.__input_data = data
-        
-    def update(self):
 
-        colors=["b", "r", "g", "y", "k", "c"]
+    def update(self):
+        colors = ["b", "r", "g", "y", "k", "c"]
         self.ax1.clear()
         self.ax2.clear()
         cat1 = self.dropdown1.currentText()
         cat2 = self.dropdown2.currentText()
-        print (cat1, cat2)
+        print(cat1, cat2)
 
         for i, value in enumerate(tips[cat1].unique()):
-            print ("value ", value)
+            print("value ", value)
             df = tips.loc[tips[cat1] == value]
             self.axes[i].set_title(cat1 + ": " + value)
             for j, value2 in enumerate(df[cat2].unique()):
-                print ("value2 ", value2)
-                df.loc[ tips[cat2] == value2 ].plot(kind="scatter", x="total_bill", y="tip", 
-                                                ax=self.axes[i], c=colors[j], label=value2)
-        self.axes[i].legend()   
-        self.graph.canvas.draw_idle()
-        
-
+                print("value2 ", value2)
+                df.loc[tips[cat2] == value2].plot(kind="scatter", x="total_bill", y="tip",
+                                                  ax=self.axes[i], c=colors[j], label=value2)
+        self.axes[i].legend()
+        self.graph.draw()
         
 if __name__ == "__main__":
     from AnyQt.QtWidgets import QApplication
