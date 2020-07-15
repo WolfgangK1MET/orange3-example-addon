2a1ada642d8b24430f4725952775a4c5181892a8
k1-simonb <67892471+k1-simonb@users.noreply.github.com>
2020-07-15 14:50:17 +0200

    Update multiple_plots_test.py

--- orangecontrib/example/widgets/multiple_plots_test.py
+++ orangecontrib/example/widgets/multiple_plots_test.py
@@ -5,7 +5,47 @@ import numpy as np
 x = np.linspace(0, 2 * np.pi, 400)
 y = np.sin(x ** 2)
 
+
+# Die Plots werden untereinander angezeigt
 fig, axs = plt.subplots(2)
 fig.suptitle("Vertically stacked subplots")
 axs[0].plot(x, y)
 axs[1].plot(x, -y)
+
+# tuple statt Liste
+fig, (ax1, ax2) = plt.subplots(2)
+fig.suptitle('Vertically stacked subplots')
+ax1.plot(x, y)
+ax2.plot(x, -y)
+
+# Die Plots werden nebeneinander angzeiegt
+fig, (ax1, ax2) = plt.subplots(1, 2)
+fig.suptitle('Horizontally stacked subplots')
+ax1.plot(x, y)
+ax2.plot(x, -y)
+
+# Es gibt 2 Reihen und je Reihe sind 2 Plots
+fig, axs = plt.subplots(2, 2)
+axs[0, 0].plot(x, y)
+axs[0, 0].set_title('Axis [0,0]')
+axs[0, 1].plot(x, y, 'tab:orange')
+axs[0, 1].set_title('Axis [0,1]')
+axs[1, 0].plot(x, -y, 'tab:green')
+axs[1, 0].set_title('Axis [1,0]')
+axs[1, 1].plot(x, -y, 'tab:red')
+axs[1, 1].set_title('Axis [1,1]')
+
+for ax in axs.flat:
+    ax.set(xlabel='x-label', ylabel='y-label')
+
+# Hide x labels and tick labels for top plots and y ticks for right plots.
+for ax in axs.flat:
+    ax.label_outer()
+
+# Die x-Ache wird geteilt, könnte wichtig werden, weil die Zeit bei jedem Plot gleich sein wird.
+
+plt.figure(1)
+fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+fig.suptitle('Aligning x-axis using sharex')
+ax1.plot(x, y)
+ax2.plot(x + 1, -y)
