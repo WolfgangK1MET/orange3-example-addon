295fefbb31e6d891abbb60474a13672e8dc71fe1
k1-simonb <67892471+k1-simonb@users.noreply.github.com>
2020-07-21 10:51:04 +0200

    Update plot_test.py

--- orangecontrib/example/widgets/plot_test.py
+++ orangecontrib/example/widgets/plot_test.py
@@ -16,8 +16,8 @@ class MainWindow(QtWidgets.QMainWindow):
         temperature = [30,32,34,32,33,31,29,32,35,45]
         self.graphWidget.setBackground('w')
 
-        # plot data: x, y values
-        self.graphWidget.plot(hour, temperature)
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.graphWidget.plot(hour, temperature, pen=pen)
 
 
 def main():
