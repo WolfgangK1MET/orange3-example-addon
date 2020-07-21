2040f0552167105b514eb9a6d02a22de60bffffc
k1-simonb <67892471+k1-simonb@users.noreply.github.com>
2020-07-21 10:51:08 +0200

    Create owpyqtplot.py

---
+++ orangecontrib/example/widgets/owpyqtplot.py
@@ -0,0 +1,63 @@
+import Orange
+
+from Orange.data import Table, ContinuousVariable
+from Orange.widgets.widget import OWWidget, Input, Output, Msg
+from Orange.data import TimeVariable
+from Orange.widgets import gui
+from Orange.widgets.utils.itemmodels import DomainModel
+from pyqtgraph.widgets.MatplotlibWidget import MatplotlibWidget
+import dateutil.parser
+import matplotlib.dates as mdates
+from mpldatacursor import datacursor
+
+from AnyQt.QtCore import Qt
+from PyQt5 import QtGui
+
+
+class OWEasyMatplot(OWWidget):
+    name = "Matplot - test"
+    description = "A widget for easy plots with matplotlib"
+    icon = "icons/oweasyplot.svg"
+    want_main_area = True
+
+    class Inputs:
+        data = Input("Data", Orange.data.Table)
+
+    class Outputs:
+        selected = Output("Selected Data", Orange.data.Table)
+
+    class Error(OWWidget.Error):
+        pass
+
+    class Warning(OWWidget.Warning):
+        empty_data = Msg("There is no data to show.")
+
+    def __init__(self):
+        super().__init__()
+
+        self.__input_data = None
+        self.Warning.empty_data(shown=True)
+
+        self.graph = MatplotlibWidget()
+
+        self.show()
+
+    @Inputs.data
+    def set_data(self, dataset):
+        self.__input_data = dataset
+
+        self.Outputs.selected.send(None)
+
+
+if __name__ == "__main__":
+    from AnyQt.QtWidgets import QApplication
+
+    table = Table("WK1_20200201.csv")
+
+    a = QApplication([])
+    df = OWEasyMatplot()
+
+    df.set_data(table)
+
+    df.show()
+    a.exec()
