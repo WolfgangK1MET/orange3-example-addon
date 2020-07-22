e6616b3125ee3413e8c0ecdc3a3c7233ce3e4ccd
k1-simonb <67892471+k1-simonb@users.noreply.github.com>
2020-07-22 13:03:44 +0200

    Update pyqtplotwidgettest.py

--- orangecontrib/example/widgets/pyqtplotwidgettest.py
+++ orangecontrib/example/widgets/pyqtplotwidgettest.py
@@ -3,6 +3,7 @@ import Orange
 from Orange.data import Table
 from Orange.widgets.widget import OWWidget, Input, Output, Msg
 import pyqtgraph as pg
+from dateaxisitem import DateAxisItem
 
 
 class OWEasyMatplot(OWWidget):
@@ -29,7 +30,7 @@ class OWEasyMatplot(OWWidget):
         self.__input_data = None
         self.Warning.empty_data(shown=True)
 
-        self.graph = pg.PlotWidget()
+        self.graph = pg.PlotWidget(axisItems = {'bottom': DateAxisItem()})
 
         # box = gui.vBox(self.mainArea, True, margin=0)
         self.mainArea.layout().addWidget(self.graph)
@@ -39,8 +40,41 @@ class OWEasyMatplot(OWWidget):
     def set_data(self, dataset):
         self.__input_data = dataset
 
+        if dataset is not None:
+            self.Warning.empty_data(shown=False)
+            time_var = self.__detect_time_variable()
+
+            self.x_model.set_domain(dataset.domain)
+            self.y_model.set_domain(dataset.domain)
+
+            # TODO: Throw exception if there is no datetime or/and number type
+            self.attr_x = time_var
+            self.attr_y0 = TableUtility.get_first_continuous_variable(self.__input_data)
+
+            # self.cid = self.graph.canvas.mpl_connect('on_click', onclick)
+            self.__update_plot()
+
         self.Outputs.selected.send(None)
 
+    def __update_plot(self):
+        print("Update Plot")
+
+        x = []
+        y = self.selected = self.__input_data[:, self.attr_y0]
+
+        for row in self.__input_data:
+            x.append(dateutil.parser.parse(f'{row["DatumUhrzeit"]}'))
+
+        n = []
+        for i, v in enumerate(x):
+            n.append([v, y[i][0]])
+            print(v, y[i][0])
+
+        n = np.array(n)
+
+        self.p1.plot(n, pen="r")
+        # self.c1.setData(n)
+        # self.c2.setData(n)
 
 if __name__ == "__main__":
     from AnyQt.QtWidgets import QApplication
