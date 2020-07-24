82684356e5475981f38039a4f315ac81c5841dd1
k1-simonb <67892471+k1-simonb@users.noreply.github.com>
2020-07-24 07:24:48 +0200

    Update owdataslicingplot.py

--- orangecontrib/example/widgets/owdataslicingplot.py
+++ orangecontrib/example/widgets/owdataslicingplot.py
@@ -136,7 +136,7 @@ class PyQtTest(OWWidget):
             dt = dateutil.parser.parse(f'{row["DatumUhrzeit"]}')
             print(dt)
             # Es ist nicht klar auf welches Datum sich die vergangenen Sekunden beziehen müssen
-            v = dt - datetime.datetime(1970, 1, 1)
+            v = dt - datetime.datetime.now()
             # Es wird auf jeden Fall falsch dargestellt.
             v = v.total_seconds()
             x.append(v)
